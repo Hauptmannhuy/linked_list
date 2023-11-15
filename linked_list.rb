d1d6bba5cffc5b2623d608f7cb2978ebda25ce1c
Hauptmannhuy <dindago2324@gmail.com>
2023-11-15 02:03:41 +0200

    implemented #get_head,#size,#at,#prepend methods

--- linked_list.rb
+++ linked_list.rb
@@ -3,7 +3,10 @@ class LinkedList
   def initialize
    @head = nil
    @tail = nil
+
   end
+
+  
   def append
     new_node = Node.new()
     if @head.nil?
@@ -18,20 +21,53 @@ class LinkedList
     end
   
   def prepend
-    
+    new_node = Node.new()
+    if @head.nil?
+      @head = new_node
+    elsif !@head.nil? && @tail.nil?
+      @tail = @head
+      @head = new_node
+      @head.next_node = @tail
+    elsif !@head.nil? && !@tail.nil?
+      old_node_refference = @head
+      @head = new_node
+      @head.next_node = old_node_refference
+    end
   end
 
-  
+def size
+  count = 0
+  current = @head    
+  while current 
+    count+=1
+    current = current.next_node
+  end
+  count
+end
+
+def get_head
+ ["Node: #{self.head.instance_variable_get(:@value)}, next node is #{self.head.next_node.instance_variable_get(:@value)}"]
+end
+
+def at (index)
+  return "Specified node doesn't exist" if index > self.size
+  current = @head
+  index.times do
+    current = current.next_node
+  end
+  current.instance_variable_get(:@value)
+end
+
 end
 
 class Node
   attr_accessor :value, :next_node
   def initialize
-    @value = nil
+    @value = rand(100).to_s
     @next_node = nil
+
   end
-  
 end
 
 list = LinkedList.new()
-list.append
+list.prepend
